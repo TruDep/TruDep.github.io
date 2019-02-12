3f9a403b7fce8e1b124aece05cb3d1f1f71d51ef
Roman M <martynenko.roman.06@gmail.com>
2019-02-12 16:17:01 +0300

    v2.01

---
+++ labwork/Martynenko_R/lab/js/lab15_martynenko.js
@@ -0,0 +1,107 @@
+
+// 1 ########################
+
+console.log('1 задание');
+console.log('_____________________________________');
+
+var login = 'user';
+console.log(window.login);  //user
+// т.к. window.login и переменная login одно и тоже
+
+var login = 'user';
+console.log(window.user);  //undefined
+// т.к. присваивание ещё не выполнено, но свойство с именем user в объекте window уже существует
+
+var login = 'user';
+console.log(window);  //Window очень большой объект который хранит в себе множество объектов текущей открытой страницы
+
+window.login='user';
+console.log(login);  //user
+// т.к. window.login и переменная login одно и тоже
+
+
+
+console.log('_____________________________________');
+// 2 ########################
+console.log('2 задание');
+console.log('_____________________________________');
+
+
+console.log( x );  //undefined
+var x = 1;
+// т.к. переменная x объявленна, но т.к. присвоение значения x=1 сделанно после вывода, то значение переменной по умолчанию = undefined
+
+console.log( y );  //y is not defined т.к. y нигде не объявлена
+y = 1;
+
+
+console.log('_____________________________________');
+// 3 ########################
+console.log('3 задание');
+console.log('_____________________________________');
+
+if ("X" in window) {
+        var X = 1;
+    }
+    console.log( X );  //undefined
+
+    //Какое значение имеет переменная Х после выполнения 1-й строки кода (undefined, 1, true, false)
+    //undefined т.к. переменная x объявленна, значение переменной по умолчанию = undefined
+
+if ("X" in window) {
+        X = 1;
+    }
+    console.log( X );  //Выведит ошибку, если в объекте window нет свойства X, оно может появится после выполнения команды из 1 примера, тогда и ошибки не будет и выведит 1
+
+if ("X" in window) {
+    X = 1;
+    }
+    var X;
+    console.log( X );  // Выведит undefined, т.к. 
+
+console.log('_____________________________________');
+// 4 ########################
+console.log('4 задание');
+console.log('_____________________________________');
+
+function S(a,b) //Function Declaration
+{
+    var S = a*b; // локальная переменная
+    return S;
+}
+
+var S2 = function(a,b) {return a*b;} //Function Expression .S2 - глобальная переменная
+
+console.log(S(2,3)) //6
+console.log(S2(2,3))  //6
+
+console.log('_____________________________________');
+
+// 5 ########################
+console.log('5 задание');
+console.log('_____________________________________');
+
+sayHi('Masha');//undefinedMasha, т.к. phrase объявлена после и её значение по умолчанию = undefined
+var phrase = "Hi, ";
+function sayHi(name) {
+    console.log(phrase + name);
+}
+
+console.log('_____________________________________');
+
+// 6 ########################
+console.log('6 задание');
+console.log('_____________________________________');
+
+var value = 0;// глобальная переменная value = 0
+function f() {
+    if (1) {//всегда выполнится
+        value = true;// локальная переменная value = 1
+    } else {
+        var value = false;//никогда не выполнится
+    }
+    console.log(value);//выводит 1, т.к. обращается к локальной переменной
+}
+f();//true
+
+console.log('_____________________________________');
